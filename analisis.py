69a0cabb4c64487cd985219ff47b7b6d0a3b7172
Getsemani <getse.gtc@gmail.com>
2020-09-13 04:11:08 -0500

    Creacion de la clase Analisis

--- analisis.py
+++ analisis.py
@@ -8,6 +8,7 @@ class Preprocesamiento:
     
     def __init__(self, diario):
         self.diario = diario
+        self.diario_prep = []
 
     def crearOraciones(self, texto):
         sentences = []
@@ -48,7 +49,7 @@ class Preprocesamiento:
             limpio.append(' '.join(frase))
             frase = []
             
-        return limpio
+        self.diario_prep = limpio
     
 
 class Analisis:
@@ -57,13 +58,25 @@ class Analisis:
     def __init__(self, diario_prep):
         self.diario_prep = diario_prep
         
+    def extraerPKL(self):
+        with open('Modelo_entrenado.pkl', 'rb') as fo:  #?
+            loaded_model = joblib.load(fo)
+        return loaded_model
         
+    def realizarAnalisis(self, diario_prep):
+        modelo = self.extraerPKL()
+        emociones = modelo.predict(diario_prep)
+        return emociones
         
 
 def main():
     contenido_diario = "Hoy empecé el día tomando un buen desayuno, con café y postre. Luego de esto fui al trabajo, soy periodista y he tenido que investigar bastante en estos días. Casi no me ha dejado tiempo para compartir con algunos amigos, pero estoy bien porque me gusta lo que hago. En la tarde cuando salía de hacer mis labores me encontré con Nick, él es mi vecino y me parece muy guapo. Me invitó a cenar, acepté y la pasamos genial. Cuando llegué a mi casa me di cuenta que se me había olvidado pagar los servicios, por lo que no tenía nada de luz. Toqué la puerta de Nick, pero al parecer se había quedado profundamente dormido. Así que tuve que improvisar al prender unas velas y estuve observando mucho por la ventana a los caminantes nocturnos, cosa que no hacía desde hace mucho. En seguida noté que había muchos vagabundos y me pregunté: ¿Qué habrá pasado para que terminaran en ese lugar? Después de no encontrar respuestas a mi pregunta me hice poco de té. Apenas y podía ver la llama de la candela. Alguien tocaba a la puerta y pude ver por el picaporte que era Nick. Me sentí muy aliviada en ese momento, así que le abrí, pudimos conversar un rato y me invitó a pasar la noche en su casa. Al día siguiente me devolví a mi hogar, al pasarla junto con mi vecino tenía muchas emociones juntas y en realidad se convirtió en la mejor noche de mi vida. Y sin más que agregar, buenas noches y hasta mañana."
     a = Preprocesamiento(contenido_diario)
-    print(a.preprocesarDiario())
+    a.preprocesarDiario()
+    resultado = Analisis(a.diario_prep)
+    print(a.diario_prep)
+    print()
+    print(resultado.realizarAnalisis(a.diario_prep))
     
     
 if __name__ == '__main__':
